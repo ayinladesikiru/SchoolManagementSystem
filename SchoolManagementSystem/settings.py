4a00c5a0e0550aed7440514e9efef865634cef8d
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2022-09-13 19:54:27 +0100

    model and database configuration

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -1,6 +1,6 @@
 import os
 
-from dotenv import load_dotenv, find_dotenv
+import environ
 
 """
 Django settings for SchoolManagementSystem project.
@@ -19,18 +19,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-load_dotenv(find_dotenv())
-SECRET_KEY = os.environ['SECRET_KEY']
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sms.apps.SmsConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,17 +74,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SchoolManagementSystem.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DATABASE_NAME',
+        'USER': 'DATABASE_USER',
+        'PASSWORD': 'DATABASE_PASSWORD',
+        'HOST': 'DATABASE_HOST',
+        'PORT': 'DATABASE_PORT',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -104,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -115,7 +116,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- sms/models.py
+++ sms/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
 
 # Create your models here.
 
@@ -12,7 +8,6 @@ class Students(models.Model):
         ('MALE', 'Male'),
         ('FEMALE', 'Female')
     )
-    student_id = models.IntegerField(primary_key=True)
     date_of_birth = models.DateField()
     first_name = models.CharField(max_length=255)
     middle_name = models.CharField(max_length=255)
@@ -22,27 +17,21 @@ class Students(models.Model):
 
 
 class Course_Scheduled(models.Model):
-    course_schedule_id = models.IntegerField(primary_key=True)
     course_offering_id = models.ForeignKey('Course_Offered', on_delete=models.CASCADE)
     other_course_schedule_details = models.TextField(max_length=255)
-    course_offered = models.ManyToOneRel('Course_Offered', on_delete=models.CASCADE, field_name='course_offered')
 
 
 class Student_Course_Registrations(models.Model):
-    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
-    course_schedule_id = models.ForeignKey(Course_Scheduled, on_delete=models.CASCADE)
-    student_course_registration = models.ManyToOneRel(Students, on_delete=models.CASCADE)
-    course_scheduled = models.ManyToOneRel(Course_Scheduled, on_delete=models.CASCADE)
+    course_schedule_id = models.ForeignKey(Course_Scheduled, on_delete=models.CASCADE, related_name='course_sch_id')
+    student_course_registration = models.ForeignKey(Students, on_delete=models.CASCADE)
 
 
 class Course_Offered(models.Model):
-    course_offering_id = models.IntegerField(primary_key=True)
     course_offering_name = models.TextField(max_length=255)
     other_course_offering_details = models.TextField(max_length=255)
 
 
 class Staff(models.Model):
-    staff_id = models.IntegerField(primary_key=True)
     date_of_birth = models.DateField()
     first_name = models.TextField(max_length=255)
     last_name = models.TextField(max_length=255)
@@ -55,18 +44,15 @@ class Staff(models.Model):
 class Staff_Courses_Supervision(models.Model):
     staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
     course_offering_id = models.ForeignKey(Course_Offered, on_delete=models.CASCADE)
-    staff = models.ManyToOneRel(Staff, on_delete=models.CASCADE)
 
 
 class Staff_Research_Interests(models.Model):
     staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
     area_of_research_id = models.ForeignKey('Research_Projects', on_delete=models.CASCADE)
-    staff = models.ManyToOneRel(Staff, on_delete=models.CASCADE)
 
 
 class Research_Projects(models.Model):
-    project_id = models.IntegerField(max_length=120, primary_key=True)
-    area_of_research_id = models.IntegerField(max_length=120)
+    area_of_research_id = models.IntegerField()
     project_name = models.TextField(max_length=255)
     project_description = models.TextField(max_length=255)
     other_details = models.TextField(max_length=255)
@@ -77,5 +63,3 @@ class Staff_on_Research_Projects(models.Model):
     staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
     date_from = models.DateField(primary_key=True)
     date_to = models.DateField()
-    research_projects = models.ManyToOneRel(Research_Projects, on_delete=models.CASCADE)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
